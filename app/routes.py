e94d3eb8f0f70354cd22b9bafaa6becbcd9bda57
gahara <madinikus@gmail.com>
2021-01-17 14:42:26 +0300

    downloadfile

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import asyncio
-from flask import request
+from flask import request, send_file
 from app import app
 
 from .utils import get_pdf_with_pypeteer
@@ -13,4 +13,4 @@ def catch_all():
     asyncio.set_event_loop(loop)
     asyncio.get_event_loop().run_until_complete(get_pdf_with_pypeteer(url))
 
-    return 'OK'
+    return send_file('page.pdf', as_attachment=True)
